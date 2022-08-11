91ac56e8d7b2ae00ccedca5e13cd2fcc042bc4a1
Ravindra Nag <ravindranag52@gmail.com>
2022-08-12 04:27:40 +0530

    feat: implement layout logic

---
+++ pages/_app.jsx
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+	const getLayout = Component.getLayout || ((page) => page)
+
+	if (Component.authRequired) {
+    // TODO implement auth logic
+	}
+
+	return getLayout(<Component {...pageProps} />)
+}
+
+export default MyApp
