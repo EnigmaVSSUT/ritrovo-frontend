b54f85f7186198601a094dd6408591ab835f18f2
Princerey <rohkumar0126@gmail.com>
2022-11-08 19:56:53 +0530

    typography added and mystyles removed

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -1,135 +1,90 @@
 import React from "react";
 import { TextField,Button,Typography,Grid,Toolbar,CssBaseline} from "@mui/material";
-import { makeStyles} from '@mui/styles' ;
 import { BsArrowRightShort } from 'react-icons/bs';
 import Image from 'next/image';
 import MainPic from '../public/img.svg';
 import logo from '../public/logo.png';
 import "@fontsource/poppins";
 
-const useStyles = makeStyles(() => ({
-	mainn:{
-	  background:'#ffffff',
-	  fontFamily:'Poppins',	
-	},
-   logo: {
-	flexGrow: "1",
-    cursor: "pointer",
-	color:'#000000',
-	fontSize:'28px',
-	fontWeight:'900',
-	paddingBottom: "10px",
-	marginLeft:'20px',
-	marginTop:'10px',
-	},
-	title: {
-		textDecoration: "none",
-		color: "#000000",
-		fontWeight:'700',
-		fontSize:'54px',
-		marginTop:32,
-		marginLeft: 32,
-		marginBottom: 40,
-		fontFamily:"poppins",
-		marginBottom :"65px",
-	  },
-	  sub: {
-		color: "#000000",
-		fontWeight:'600',
-		fontSize:'20px',
-		marginTop:32,
-		marginLeft: 32,
-		fontFamily:"poppins"
-	  },
-	  tf: {
-		marginLeft:32,
-		marginRight:32,
-		borderRadius:'10px',
-		background:'#FAFBFF',
-		fontFamily:"poppins"
-	  },
-	  sub2: {
-		color: "#000000",
-		fontWeight:'500',
-		fontSize:'18px',
-		marginTop:24,
-		marginLeft: 32,
-		marginBottom:36,
-		fontFamily:"poppins"
-	  },
-	link1: {
-		textDecoration: "none",
-		color: "#ffffff",
-		background:'#287EFF',
-		borderRadius:'18px',
-		fontWeight:'500',
-		fontSize:'30px',
-		padding:5.6,
-		marginTop:21.6,
-		marginRight:32,	
-		marginLeft: 32,
-		marginBottom: 16,
-		boxShadow: '1px 8px 7px 0px #287EFF80',
-		"&:hover": {
-		  color: "#bfbfbf",
-		  fontFamily:"poppins"
-	  },
-	},
-	new: {
-		color: "#000000",
-		fontWeight:'500',
-		fontSize:'18px',
-		marginTop:40,
-		marginLeft: 32,
-		fontFamily:"poppins"
-	  },
-	  img:{
-		position:"relative",
-		bottom:"40px"
-	  }
-  }));
-  
 const Signin = () => {
-	
-	const classes = useStyles();
+
     
 	return (
 		
-		<Grid className={classes.mainn}>
+		<Grid sx={{
+			backgroundColor:'background.default',
+		fontFamily:'Poppins',	
+
+		}}>
 			<getTheme/>
 			<CssBaseline />
 			<Toolbar sx={{
 				marginTop:"25px",marginLeft:"16px",
 				position:"sticky"}}>
 			<Image src={logo} alt="IMAGE" height="40px" width="40px"/>
-			<Typography variant="h4" className={classes.logo}>
+			<Typography variant="title3" >
 			 Ritrovo
 			</Typography>
 			</Toolbar>
 			
 		   <Grid container height="100%">
 		   <Grid item xs={12} md={5.7} mx="10px">
-			<Typography variant="h3" gutterBottom className={classes.title}>Sign In</Typography>
+			<Typography variant="title4" gutterBottom >Sign In</Typography><br/>
 			
-				<Typography variant="h5" gutterBottom className={classes.sub}>Username/Email</Typography>
-				<TextField size="small"  className={classes.tf} sx={{
+				<Typography variant="stitle1">Username/Email</Typography><br/>
+				<TextField size="small"  sx={{
+					width:{
+						position:"relative",
+						xs:350,
+						sm:480
+					},
+						left:32,
+						top:75,
+		right:32,
+		borderRadius:'10px',
+		backgroundColor:'gray.textf',
+		fontFamily:"poppins"
+					}}/><br/>
+				
+				<Typography variant="stitle3" gutterBottom >Password</Typography>
+				<br/><TextField size="small"  type="password"  sx={{
 					width:{
 						xs:350,
 						sm:480,
-					}}
-					}/>
-				<Typography variant="h5" gutterBottom className={classes.sub}>Password</Typography>
-				<TextField size="small"  type="password" className={classes.tf} sx={{
-					width:{
-						xs:350,
-						sm:480,
-					}}
-					}/>
-				<Typography variant="subtitle1"  gutterBottom className={classes.sub2}>Forgot Password ?</Typography>
-				<Button variant="contained" className={classes.link1}><BsArrowRightShort fontSize="40px"/> </Button>
-				<Typography variant="subtitle1" gutterBottom className={classes.new}>New User ? <a href="#"> Sign Up</a></Typography>	
+					},
+					left:32,
+		right:32,
+		top:100,
+		borderRadius:'10px',
+		backgroundColor:'gray.textf',
+		fontFamily:"poppins"
+				}
+					}/><br/>
+				<Typography variant="stitle2"  gutterBottom>Forgot Password ?</Typography>
+				<br/><Button variant="contained" sx={{
+					textDecoration: "none",
+					color: "background.default",
+					backgroundColor:'blue.primary',
+					borderRadius:'18px',
+					fontWeight:'500',
+					fontSize:'30px',
+					padding:"5.6px",
+					marginTop:"200px",
+					marginRight:"32px",	
+					marginLeft: "32px",
+					marginBottom: "16px",
+					boxShadow: '1px 8px 7px 0px #287EFF80',
+					"&:hover": {
+					  color: "gray.secondary",
+					  fontFamily:"poppins"
+				  }
+				}}><BsArrowRightShort fontSize="40px"/> </Button><br/>
+				<Typography variant="stitle" gutterBottom >New User ? <a href="#"> Sign Up</a></Typography>	
 			</Grid>
-			<Grid item md={6} className={classes.img} display={{ xs: 'none', sm:'none', md: 'none' ,lg:'block'}} >
+			<Grid item md={6} sx={{
+				position:"relative",
+				bottom:"40px"
+			}} display={{ xs: 'none', sm:'none', md: 'none' ,lg:'block'}} >
 			<Image src={MainPic} alt="IMAGE" height="640px" width="700px" />
 			</Grid>
 		</Grid>
--- theme/options/typography.js
+++ theme/options/typography.js
@@ -13,7 +13,162 @@ const typography = {
 	fontWeightLight: 300,
 	fontWeightRegular: 400,
 	fontWeightMedium: 500,
-	fontWeightBold: 700
+	fontWeightBold: 700,
+	head: {
+        flexGrow: "1",
+        cursor: "pointer",
+        fontSize:'32px',
+        fontWeight:'600',
+        lineHeight:'48px',
+        fontFamily:"poppins"
+        },
+		text:{
+			fontFamily:"poppins"
+		},
+		sub:{
+			position:'relative',
+			fontSize:25,
+			lineHeight:4,
+			fontWeight:600,
+			right:50,
+		  fontFamily:"poppins",
+		  marginLeft:'2px',
+				  
+		},
+		sub1:{
+				position:"relative",
+				top:-40,
+				right:30,
+				cursor:"crosshair",		
+		}
+		,
+		title:{
+			position:'relative',
+			flexGrow: "1",
+			fontSize:20,
+			fontWeight:'600',
+			paddingBottom: "10px",
+			top:'130px',
+			lineHeight:'30px',
+			fontFamily:"poppins"
+		},
+		title1: {
+			position:'relative',
+			flexGrow: "1",
+			fontSize:20,
+			fontWeight:'400',
+			paddingBottom: "10px",
+			top:'90px',
+			lineHeight:'20px',
+			fontFamily:"poppins",
+			left:'460px'
+			},
+			title3: {
+				flexGrow: "1",
+				cursor: "pointer",
+				fontSize:'28px',
+				fontWeight:'900',
+				paddingBottom: "10px",
+				marginLeft:'20px',
+				marginTop:'10px',
+				},
+				
+	    title4: {
+			position:"relative",
+		textDecoration: "none",
+		fontWeight:'700',
+		fontSize:'54px',
+		top:30,
+		left: 32,
+		fontFamily:"poppins",
+		marginBottom :"65px",
+	  },
+	  stitle: {
+		position:"relative",
+		fontWeight:'500',
+		fontSize:'18px',
+		top:40,
+		left: 32,
+		fontFamily:"poppins"
+	  },
+	  stitle1: {
+		position:"relative",
+		fontWeight:'600',
+		fontSize:'20px',
+		top:68,
+		left: 32,
+		fontFamily:"poppins"
+	  },
+	  stitle3: {
+		position:"relative",
+		fontWeight:'600',
+		fontSize:'20px',
+		top:95,
+		left: 32,
+		fontFamily:"poppins"
+	  },
+	  stitle2: {
+		position:"relative",
+		fontWeight:'500',
+		fontSize:'18px',
+		top:130,
+		left: 32,
+		fontFamily:"poppins"
+	  },
+	  nav: {
+		textDecoration: "none",
+		fontWeight: '600',
+		fontSize: '20px',
+		marginTop: 20,
+		marginRight: 32,
+		marginLeft: 56,
+		marginBottom: 16,
+		},
+		nav1: {
+			textDecoration: "none",
+			borderRadius: '15px',
+			fontWeight: '500',
+			fontSize: '20px',
+			paddingTop: 8,
+			paddingLeft: 32,
+			paddingBottom: 8,
+			paddingRight: 32,
+			marginTop: 12,
+			marginRight: 32,
+			marginLeft: 56,
+			marginBottom: 16,
+			
+		},
+		logo: {
+			flexGrow: "1",
+			cursor: "pointer",
+			fontSize: '28px',
+			fontWeight: 900,
+			paddingBottom: "10px",
+			marginLeft: '20px',
+			marginTop: "10px"
+		},
+		drawer: {
+			textDecoration: "none",
+			fontWeight: '600',
+			fontSize: '20px',
+			marginTop: 20,
+			marginBottom:16,
+		  },
+		  drawer1: {
+			position: "relative",
+			right: "8px",
+			textDecoration: "none",
+			borderRadius: '15px',
+			fontWeight: '500',
+			fontSize: '17.5px',
+			paddingTop: 8,
+			paddingLeft:32,
+			paddingBottom: 8,
+			paddingRight:32,
+			marginTop: 12,
+			marginBottom: 16,
+		  },
 }
 
 export default typography
