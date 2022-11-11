f4247ee1e9894d83c764762f45c73fcbd636b14b
Princerey <rohkumar0126@gmail.com>
2022-11-11 23:14:09 +0530

    left sidebar styling

--- components/Sidebar/Sidebar.jsx
+++ components/Sidebar/Sidebar.jsx
@@ -1,10 +1,11 @@
-import { Adb } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 import { Toolbar } from "@mui/material";
 import { Drawer } from "@mui/material";
 import React from "react";
 import SidebarAccount from "./SidebarAccount";
 import SidebarMenu from "./SidebarMenu";
+import Logo from "../../public/logo.png";
+import Image from 'next/image';
 
 const Sidebar = () => {
 
@@ -16,12 +17,12 @@ const Sidebar = () => {
 			PaperProps={{
                 sx:{
                     backgroundColor:"background.default",
-                    width:"220px"
+                    width:"287px"
                 }}}
 			>
-			<Toolbar>
-				<Adb />
-				<Typography fontWeight={700}>Ritrovo</Typography>
+			<Toolbar sx={{marginTop:"15px",marginLeft:"-10px"}}>
+			<Image src={Logo} alt="IMAGE" height="38px" width="38px"/>
+				<Typography sx={{marginLeft:"20px", fontSize:"23px", fontWeight:"700"}}>Ritrovo</Typography>
 			</Toolbar>
 			<SidebarMenu />
 			<SidebarAccount />
@@ -29,4 +30,4 @@ const Sidebar = () => {
 	);
 };
 
-export default Sidebar;
+export default Sidebar;
