ba409c659df76e337cda93c6c90c035489941a4e
Princerey <rohkumar0126@gmail.com>
2022-11-11 23:15:44 +0530

    changes

--- components/Sidebar/SidebarAccount.jsx
+++ components/Sidebar/SidebarAccount.jsx
@@ -1,4 +1,4 @@
-import { List } from "@mui/material";
+import { List, Typography } from "@mui/material";
 import { ListSubheader } from "@mui/material";
 import { Divider } from "@mui/material";
 import { Avatar } from "@mui/material";
@@ -6,20 +6,21 @@ import { ListItem } from "@mui/material";
 import { ListItemButton } from "@mui/material";
 import { ListItemAvatar } from "@mui/material";
 import { ListItemText } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 import React from "react";
+import "@fontsource/poppins";
+
 
 const SidebarAccount = () => {
 	return (
 		<>
-			<ListSubheader>Account</ListSubheader>
-			<Divider variant="middle" />
 			<List>
-                <ListItem disablePadding>
+                <ListItem>
                     <ListItemButton component="a" href='#'>
-                        <ListItemAvatar>
-                            <Avatar alt='John Doe' src='/' />
-                        </ListItemAvatar>
-                        <ListItemText primary="John Doe" secondary="@johndoe_"/>
+                  <Typography sx=
+                  {{fontSize:"5px"}}>
+                    <AddIcon alt='Add' src='/' sx={{color:"#fff", background:"#287EFF",borderRadius:"50%", marginRight:"25px",padding:"3px",boxShadow: "1px 3px 4px rgba(40, 126, 255, 0.64)"}}/></Typography>  
+                        <Typography sx={{color:"#B3B3B3",fontFamily:"poppins",fontWeight:600,fontSize:"16px"}}>Post</Typography>
                     </ListItemButton>
                 </ListItem>
 			</List>
@@ -27,4 +28,4 @@ const SidebarAccount = () => {
 	);
 };
 
-export default SidebarAccount;
+export default SidebarAccount;
