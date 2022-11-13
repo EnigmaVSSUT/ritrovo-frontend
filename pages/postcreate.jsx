ce0108b2df392bbaf67fe3e413477beb54d7a652
Ananya <99496164+AnanyaM007@users.noreply.github.com>
2022-11-13 11:04:35 +0530

    changes

--- pages/postcreate.jsx
+++ pages/postcreate.jsx
@@ -15,7 +15,7 @@ const PostCreate = () => {
 
 			<Grid height='730px' sx={{
 				backgroundColor: 'gray.main',
-				width: "50rem",
+				width: "71%",
 				borderRadius: 5,
 				padding: 0,
 				margin: 0
